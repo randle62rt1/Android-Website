ef39a7aa385cfec5d8036f585fbacb4e4376c4d0
Brodie Davis <bdavis9d@gmail.com>
2017-11-28 21:37:01 -0500

    add bootstrap template to page

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', index);
 app.use('/users', users);
